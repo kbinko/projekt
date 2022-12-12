6672beb4641282fcf590fff6a49c05e804ce20f8
Konrad Binko <19484@student.ans-elblag.pl>
2022-12-12 10:04:33 +0100

    2

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const burgerMenu = document.querySelector('.burger-menu');
-const nav = document.querySelector('nav');
+const navMenu = document.querySelector('.nav-menu');
+const welcome = document.querySelector('.welcome');
+const center = document.querySelector('.center');
+
+burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    welcome.classList.toggle('active');
+    center.classList.toggle('active');
+});
+
+document.querySelectorAll('.link').forEach(n => {
+    n.addEventListener('click', () => {
+        burgerMenu.classList.remove('active');
+        nav.classList.remove('active');
+        welcome.classList.remove('active');
+        center.classList.remove('active');
+    })
+})
