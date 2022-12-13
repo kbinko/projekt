0b4168c79e279f5b5787f62f3f2d07ea1ff446ce
Konrad Binko <19484@student.ans-elblag.pl>
2022-12-13 07:47:26 +0100

    2

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const navMenu = document.querySelector('.nav-menu');
+const linkNav = document.querySelectorAll('.link-nav');
 const welcome = document.querySelector('.welcome');
-const center = document.querySelector('.center');
 
 burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('active');
     navMenu.classList.toggle('active');
     welcome.classList.toggle('active');
-    center.classList.toggle('active');
+    linkNav.classList.toggle('active');
 });
 
 document.querySelectorAll('.link').forEach(n => {
@@ -15,6 +15,20 @@ document.querySelectorAll('.link').forEach(n => {
         burgerMenu.classList.remove('active');
         nav.classList.remove('active');
         welcome.classList.remove('active');
-        center.classList.remove('active');
+        linkNav.classList.remove('active');
     })
 })
+
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenEmelents = document.querySelectorAll('.hidden');
+hiddenEmelents.forEach((el) => observer.observe(el));
